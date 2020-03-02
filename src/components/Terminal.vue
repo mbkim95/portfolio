0dea4e4d5e6ea33f83d33cc7b7eaad17a5e243ea
mbkim95 <mbkim95@naver.com>
2020-03-03 01:12:20 +0900

    Implement terminal background

---
+++ src/components/Terminal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="terminal" id="page-wrapper">
+    <div id="window"></div>
+    <div class="text-left" id="content">
+      <span class="ml-2" id="id">
+        mbkim@github
+      </span>
+      <span>:</span>
+      <span style="color: #729FCF">~</span>
+      <span class="mr-2">$</span>
+      <span>{{ terminalText }}</span>
+      <span class="ml-1">{{ cursor }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Terminal",
+  props: {},
+  data() {
+    return {
+      terminalText: "Welcome To My HomePage",
+      cursor: "_"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#page-wrapper {
+  padding-left: 50px;
+}
+
+.terminal {
+  height: 100vh;
+}
+
+.terminal #window {
+  background-color: #4c4840;
+  height: 25px;
+}
+
+.terminal #content {
+  height: calc(100vh - 25px);
+  background-color: rgb(48, 10, 36, 0.8);
+}
+
+.terminal #id {
+  color: #8ae234;
+}
+</style>
