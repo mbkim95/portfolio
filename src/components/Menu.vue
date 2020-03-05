bdc085501f43e6c5b8d1615d158e26b5279ab550
mbkim95 <mbkim95@naver.com>
2020-03-05 15:49:35 +0900

    Apply Bootstrap at Menu.vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="menu">
-    <div id="page-wrapper">
-      <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-          <li class="sidebar-brand" v-for="(menu, index) in menus" :key="index">
-            <a href="#" :title="menu.value">
-              <img :src="menu.image" alt="" @click="sendMenu(menu.value)"/>
-            </a>
-          </li>
-        </ul>
-      </div>
+  <div class="menu container-fluid px-0">
+    <div class="sidebar-nav h-100">
+      <ul class="list-unstyled">
+        <li class="nav-item" v-for="(menu, index) in menus" :key="index">
+          <a class="nav-link pt-4 pb-4 px-0" href="#" :title="menu.value">
+            <img :src="menu.image" alt="" @click="sendMenu(menu.value)" />
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -52,37 +50,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#sidebar-wrapper {
-  position: fixed;
-  width: 50px;
-  height: 100%;
-  /* margin-left: -250px; */
-  background: rgba(73, 72, 72, 0.582);
-  overflow-x: hidden;
-  overflow-y: auto;
-}
-
-#page-content-wrapper {
-  width: 100%;
-  padding: 20px;
-}
-
 .sidebar-nav {
+  position: fixed;
   width: 50px;
   margin: 0;
   padding: 0;
-  list-style: none;
+  background: rgba(73, 72, 72, 0.582);
 }
 
 .sidebar-nav li a:hover {
   color: #fff;
   display: block;
   background: rgba(255, 255, 255, 0.2);
-}
-
-.sidebar-nav > .sidebar-brand {
-  font-size: 1.3em;
-  line-height: 3em;
 }
 
 .sidebar-nav li a img {
